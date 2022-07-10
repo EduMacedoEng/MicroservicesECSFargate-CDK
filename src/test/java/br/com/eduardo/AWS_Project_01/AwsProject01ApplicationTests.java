8bafd6f5b597a43a77c6975a8402e1da855bf10c
Eduardo Macedo <eduardomacedo.eng@hotmail.com>
2022-07-10 15:27:38 -0300

    Project created

---
+++ src/test/java/br/com/eduardo/AWS_Project_01/AwsProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.AWS_Project_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
